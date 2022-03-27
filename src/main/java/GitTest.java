198d4cba9f5e2f84b12a2fb82c033b8ebeeb47b6
outp <outp@augurit.com>
2022-03-27 16:51:29 +0800

    master commit2

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -3,5 +3,6 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("hello git!");
         System.out.println("hello git hot-fix!");
+        System.out.println("hello git master-modified!");
     }
 }
