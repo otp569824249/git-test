c1c0076f692d23c9b932ec7b61db4b35dbfc519a
outp <outp@augurit.com>
2022-03-27 17:28:57 +0800

    ssh push

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello git hot-fix!");
         System.out.println("hello git master-modified!");
         System.out.println("hello git hot-fix commit2!");
+        System.out.println("hello git ssh push!");
     }
 }
